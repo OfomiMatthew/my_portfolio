bd1ba219506e8445a5a79db8b3224d73349ded48
MatthewOfomi <ofomimatthew7@gmail.com>
2024-01-20 22:58:04 +0100

    first commit

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,12 @@
+import Nav from "./components/Nav"
+
+
+const App = () => {
+  return (
+    <div className="p-[20px]">
+  <Nav />
+  </div>
+  )
+}
+
+export default App
---
+++ portfolio/src/components/Nav.jsx
@@ -0,0 +1,95 @@
+import { useState } from "react";
+import { logo } from "../assets";
+
+const Nav = () => {
+  const [toggle, setToggle] = useState(false);
+  return (
+    <div>
+      <div className="flex justify-between ">
+        <img src={logo} alt="" className="h-[20px]" />
+        <div className="hidden md:flex">
+          <ul className="flex flex-row">
+            <li className="mr-4 transition-all duration-350 ease-in-out hover:scale-110 cursor-pointer hover:border-b-[1px]">
+              Home
+            </li>
+            <li className="mr-4 transition-all duration-350 ease-in-out hover:scale-110 cursor-pointer hover:border-b-[1px]">
+              About Me
+            </li>
+            <li className="mr-4 transition-all duration-350 ease-in-out hover:scale-110 cursor-pointer hover:border-b-[1px]">
+              Skills
+            </li>
+            <li className="mr-4 transition-all duration-350 ease-in-out hover:scale-110 cursor-pointer hover:border-b-[1px]">
+              Portfolio
+            </li>
+            <li className="mr-4 transition-all duration-350 ease-in-out hover:scale-110 cursor-pointer hover:border-b-[1px]">
+              Contact Me
+            </li>
+          </ul>
+        </div>
+        <div className="md:hidden lg:hidden">
+          {!toggle ? (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="w-6 h-6 cursor-pointer"
+              onClick={() => {
+                setToggle(true);
+              }}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5"
+              />
+            </svg>
+          ) : (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="w-6 h-6 cursor-pointer float-right"
+              onClick={() => {
+                setToggle(false);
+              }}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M6 18 18 6M6 6l12 12"
+              />
+            </svg>
+          )}
+
+          {toggle ? (
+            <ul className="flex flex-col mt-8 p-4 bg-gray-500 rounded-2xl">
+              <li className="p-2 ml-6 transition-all duration-350 ease-in-out hover:scale-110 cursor-pointer">
+                Home
+              </li>
+              <li className="p-2 ml-6 transition-all duration-350 ease-in-out hover:scale-110 cursor-pointer">
+                About Me
+              </li>
+              <li className="p-2 ml-6 transition-all duration-350 ease-in-out hover:scale-110 cursor-pointer">
+                Skills
+              </li>
+              <li className="p-2 ml-6 transition-all duration-350 ease-in-out hover:scale-110 cursor-pointer">
+                Portfolio
+              </li>
+              <li className="p-2 ml-6 transition-all duration-350 ease-in-out hover:scale-110 cursor-pointer">
+                Contact Me
+              </li>
+            </ul>
+          ) : (
+            "none"
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Nav;
